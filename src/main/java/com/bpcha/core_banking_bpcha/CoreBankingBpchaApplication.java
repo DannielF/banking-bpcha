802b7f00b2826c33b0f6c50b84506dae0db4880a
daniel.granados <daniel.granados@sofka.com.co>
2023-05-15 08:53:02 -0500

    Project started

---
+++ src/main/java/com/bpcha/core_banking_bpcha/CoreBankingBpchaApplication.java
@@ -0,0 +1,13 @@
+package com.bpcha.core_banking_bpcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreBankingBpchaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreBankingBpchaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bpcha/core_banking_bpcha/CoreBankingBpchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpcha.core_banking_bpcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBankingBpchaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
