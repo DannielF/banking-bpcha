bacbfd11149a1e560d9c5cf773f9c8bffc36a881
daniel.granados <daniel.granados@sofka.com.co>
2023-05-16 10:05:45 -0500

    upd: ClientDataRepository access method

--- src/main/java/com/bpcha/core_banking_bpcha/infrastructure/sql_repository/jpa_mysql/client/data/ClientDataRepository.java
+++ src/main/java/com/bpcha/core_banking_bpcha/infrastructure/sql_repository/jpa_mysql/client/data/ClientDataRepository.java
@@ -8,5 +8,5 @@ public interface ClientDataRepository extends CrudRepository<ClientData, Integer
 
     @Transactional(readOnly = true)
     @Query("select c from ClientData c where c.state = 'TRUE'")
-    public ClientData getClients();
+    ClientData getClients();
 }
