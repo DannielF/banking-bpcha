244837d8f9f7871935d688236574e3ed6d006897
daniel.granados <daniel.granados@sofka.com.co>
2023-05-16 11:57:53 -0500

    upd: ClientRepositoryAdapter.java

--- src/main/java/com/bpcha/core_banking_bpcha/infrastructure/sql_repository/jpa_mysql/client/adapter/ClientRepositoryAdapter.java
+++ src/main/java/com/bpcha/core_banking_bpcha/infrastructure/sql_repository/jpa_mysql/client/adapter/ClientRepositoryAdapter.java
@@ -2,42 +2,53 @@ package com.bpcha.core_banking_bpcha.infrastructure.sql_repository.jpa_mysql.cli
 
 import com.bpcha.core_banking_bpcha.domain.model.client.Client;
 import com.bpcha.core_banking_bpcha.domain.model.client.gateway.ClientRepository;
+import com.bpcha.core_banking_bpcha.domain.model.shared.BusinessException;
 import com.bpcha.core_banking_bpcha.infrastructure.sql_repository.jpa_mysql.client.ConverterClient;
 import com.bpcha.core_banking_bpcha.infrastructure.sql_repository.jpa_mysql.client.data.ClientDataRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
 public class ClientRepositoryAdapter implements ClientRepository {
 
     private final ClientDataRepository repository;
-    private final ConverterClient converterClient;
 
     @Override
     public Client saveClient(Client client) {
-        return null;
+        var clientData = ConverterClient.toData(client);
+        return ConverterClient.toEntity(repository.save(clientData));
     }
 
     @Override
     public Client findClientById(Integer id) {
-        return null;
+
+        var clientData = repository.findById(id);
+        if (clientData.isPresent()) return ConverterClient.toEntity(clientData.get());
+        throw new BusinessException("404");
     }
 
     @Override
     public List<Client> clientList() {
-        return null;
+        var clientList = repository.getClients();
+        return Arrays.stream(clientList).map(ConverterClient::toEntity).collect(Collectors.toList());
     }
 
     @Override
     public Client deleteClient(Integer id) {
-        return null;
+        var clientData = repository.findById(id);
+        clientData.ifPresent(data -> repository.logicDeleteClient(data.getId()));
+        throw new BusinessException("YOU HAS DELETED THE WHOLE DB");
     }
 
     @Override
     public Client updateClient(Client client) {
-        return null;
+        var clientData = repository.findById(client.getId());
+        clientData.ifPresent(repository::save);
+        throw new BusinessException("It couldn't be updated");
     }
 }
--- src/main/java/com/bpcha/core_banking_bpcha/infrastructure/sql_repository/jpa_mysql/client/data/ClientDataRepository.java
+++ src/main/java/com/bpcha/core_banking_bpcha/infrastructure/sql_repository/jpa_mysql/client/data/ClientDataRepository.java
@@ -1,7 +1,9 @@
 package com.bpcha.core_banking_bpcha.infrastructure.sql_repository.jpa_mysql.client.data;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface ClientDataRepository extends CrudRepository<ClientData, Integer> {
@@ -9,4 +11,9 @@ public interface ClientDataRepository extends CrudRepository<ClientData, Integer
     @Transactional(readOnly = true)
     @Query("select c from ClientData c where c.state = 'TRUE'")
     ClientData[] getClients();
+
+    @Transactional
+    @Modifying
+    @Query("update ClientData cld set cld.state = 'FALSE' where cld.id = :id")
+    void logicDeleteClient(@Param("id") Integer id);
 }
