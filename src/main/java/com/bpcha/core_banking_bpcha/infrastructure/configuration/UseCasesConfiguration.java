64477ef54b3f844c02a192c95f45b89718efc3f2
daniel.granados <daniel.granados@sofka.com.co>
2023-05-16 15:46:30 -0500

    upd: UseCasesConfiguration, application.yml

--- src/main/java/com/bpcha/core_banking_bpcha/infrastructure/configuration/UseCasesConfiguration.java
+++ src/main/java/com/bpcha/core_banking_bpcha/infrastructure/configuration/UseCasesConfiguration.java
@@ -1,13 +1,15 @@
 package com.bpcha.core_banking_bpcha.infrastructure.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(basePackages = "com.bpcha.core_banking_bpcha",
+@RequiredArgsConstructor
+@ComponentScan(basePackages = "com.bpcha.core_banking_bpcha.domain.usecase",
         includeFilters = {
-                @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".+UseCase$")
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
         }, useDefaultFilters = false)
 public class UseCasesConfiguration {
 }
